2e3db542a958c2c5950570babcb7074c7400ca64
IanD1013 <ian.tung1013@gmail.com>
2024-10-22 19:09:33 +1300

    Configure tailwind and add logo

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./sections/Header.jsx";
+
+export default function App() {
+    return (
+        <main className="overflow-hidden">
+            <Header />
+
+        </main>
+    )
+}
