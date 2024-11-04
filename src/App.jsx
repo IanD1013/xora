e0575b00fe2cf88be6238d4b2f2b6ee78866b09b
IanD1013 <ian.tung1013@gmail.com>
2024-11-05 10:15:23 +1300

    Add Download section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
 import Testimonials from "./sections/Testimonials.jsx";
+import Download from "./sections/Download.jsx";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Pricing />
       <Faq />
       <Testimonials />
+      <Download />
     </main>
   );
 }
